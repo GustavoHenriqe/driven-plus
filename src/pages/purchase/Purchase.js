a16b315ad7cf19bdae36f2d7fa97b794eabe0ceb
Gustavo Henrique <binarymaster100@gmail.com>
2022-12-27 17:56:37 -0300

    Purchase Quase Concluido

--- src/pages/purchase/Purchase.js
+++ src/pages/purchase/Purchase.js
@@ -1,7 +1,8 @@
-
+import { MainStyled } from "./styledPurchase"
+import Pointer from "../../assets/seat.png"
 
 import { useContext, useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 import axios from "axios"
 import ApiServer from "../../api/API"
@@ -26,7 +27,19 @@ function Purchase() {
 
     return (
         <>
-
+            <MainStyled colorTitle={color => { if(_planeObject.id == 2){ return "#FFF16F" }else if (_planeObject.id == 3){ return "#56D59F"}
+            }}>
+                <header>
+                    <Link to="/subscriptions">
+                        <img src={Pointer}></img>
+                    </Link>
+                    <img src={_planeObject.image}></img>
+                    <h1>{_planeObject.name}</h1>
+                </header>
+                <main>
+                    
+                </main>
+            </MainStyled>
         </>
     )
 }
--- src/pages/purchase/styledPurchase.js
+++ src/pages/purchase/styledPurchase.js
@@ -1,2 +1,25 @@
 import styled from "styled-components";
 
+export const MainStyled = styled.main`
+    width: 375px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 87px;
+    margin-bottom: 22px;
+    
+    h1{
+        margin-top: 12px;
+        font-weight: 700;
+        font-size: 32px;
+        color: ${props => props.colorTitle};
+    }
+
+    a{
+        position: fixed;
+        left: 22px;
+        top: 22px;
+        z-index: 10;
+    }
+`
